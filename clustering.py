47da145e93305459f193f09f121154f4b2276df4
epvolchok <e.p.volchok@gmail.com>
2025-04-18 21:40:48 +0200

    saving data

--- clustering.py
+++ clustering.py
@@ -34,10 +34,57 @@ mean_dist = distances[np.triu_indices_from(distances, k=1)].mean()
 
 print("Средняя косинусная дистанция между эмбеддингами:", mean_dist)
 
-features_scaled = StandardScaler().fit_transform(features)
-model_pca = PCA(n_components=0.95, random_state=42)
-features_PCA = model_pca.fit_transform(features_scaled)
+
+features_processed = obj.preproccess('PCA+UMAP10D')
+
+# 🔎 Кластеризация HDBSCAN
+clusterer = hdbscan.HDBSCAN(min_cluster_size=15, metric='euclidean')
+labels = clusterer.fit_predict(features_processed)
+
+# ℹ️ Вывод числа кластеров
+n_clusters = len(set(labels)) - (1 if -1 in labels else 0)
+print(f"Найдено кластеров: {n_clusters}")
+
+df = obj.create_database(labels)
+obj.sort_files(df)
+
+"""
+#features_scaled = StandardScaler().fit_transform(features)
+model_pca = PCA(n_components=0.95, svd_solver='full')
+features_PCA = model_pca.fit_transform(features)
 print(features_PCA.shape)
+
+
+model_umap = umap.UMAP(n_components=10, min_dist=0.1, metric='cosine')
+features_umap = model_umap.fit_transform(features_PCA)
+
+
+reducer = umap.UMAP(n_components=2,  metric='cosine')
+umap_2d = reducer.fit_transform(features)
+
+# 🔎 Кластеризация HDBSCAN
+clusterer = hdbscan.HDBSCAN(min_cluster_size=15, metric='euclidean')
+labels = clusterer.fit_predict(umap_2d)
+
+# ℹ️ Вывод числа кластеров
+n_clusters = len(set(labels)) - (1 if -1 in labels else 0)
+print(f"Найдено кластеров: {n_clusters}")
+
+# 🎨 Визуализация
+plt.figure(figsize=(10, 8))
+palette = plt.get_cmap("tab10")
+
+for label in set(labels):
+    mask = labels == label
+    color = 'gray' if label == -1 else palette(label % 10)
+    plt.scatter(umap_2d[mask, 0], umap_2d[mask, 1], s=10, color=color, label=f'Кластер {label}' if label != -1 else 'Шум')
+
+plt.legend()
+plt.title("UMAP + HDBSCAN: визуализация кластеров")
+plt.xlabel("UMAP-1")
+plt.ylabel("UMAP-2")
+plt.show()
+"""
 """
 features_scaled = StandardScaler().fit_transform(features)
 model_pca = PCA(n_components=0.95, random_state=42)
--- libcluster.py
+++ libcluster.py
@@ -5,19 +5,23 @@ from sklearn.cluster import KMeans
 from sklearn.manifold import TSNE
 from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import Pipeline
 import umap
 from torchvision import models, transforms
 import torch
 from tqdm import tqdm
 import matplotlib.pyplot as plt
 import hdbscan
-import json
+import re
+import datetime
+import pandas as pd
 
 class Clustering:
 
     def __init__(self, path, device='cuda'):
         self._device = torch.device(device if torch.cuda.is_available() else 'cpu')
         print(f'The device is {self._device}')
+        self.names = [f for f in os.listdir(path) if Clustering.check_extension(f)]
         self.img_path = [path + f for f in os.listdir(path) if Clustering.check_extension(f)]
         self.features = []
 
@@ -42,6 +46,28 @@ class Clustering:
         model_resnet50.to(self._device)
 
         return model_resnet50, transform
+    
+    @staticmethod
+    def models_dict():
+        models_dict = {
+            'pca': PCA(n_components=0.95, svd_solver='full'),
+            'scaler': StandardScaler(),
+            'umap10d': umap.UMAP(n_components=10, min_dist=0.1, metric='cosine'),
+            'umap2d': umap.UMAP(n_components=2)
+        }
+        return models_dict
+
+    def preproccess(self, pipe_str): #pipe_str='PCA+UMAP+..'
+        models = pipe_str.lower().split('+')
+        pipes = []
+        for m in models:
+            pipes.append((m, Clustering.models_dict()[m]))
+        if self.features.size:
+            pipe = Pipeline(pipes)
+            results = pipe.fit_transform(self.features)
+            return results
+        else:
+            return None
     
     def feature_extraction(self):
         
@@ -72,7 +98,47 @@ class Clustering:
     def read_features(self, path='./features.npy'):
         try:
             data = np.load(path)
-            return data
+            self.features = data
+            return self.features
         except:
             print('Error during reading data')
             return None
+    
+    @staticmethod
+    def extract_observ_data(path):
+        template = r'(r|t)swf-e_(\d{4})(\d{2})(\d{2})'
+        search = re.search(template, path)
+        observation_type = search.group(1)
+        date = datetime.date(int(search.group(2)), int(search.group(3)), int(search.group(4)))
+        return observation_type, date
+    
+    def create_database(self, labels):
+        num = len(labels)
+        types = []
+        dates = []
+        for name in self.names:
+            type_, date_ = Clustering.extract_observ_data(name)
+            types.append(type_)
+            dates.append(date_)
+        df = pd.DataFrame({
+            'filename': self.names,
+            'observation type': types,
+            'date': dates,
+            'labels': labels,
+            'path': self.img_path
+        })
+        print(df.head())
+        return df
+    def create_dirs(df):
+        if not os.path.exists('./processed'):
+            os.makedirs('my_folder')
+        
+
+
+    def sort_files(self, df):
+        for i, row in enumerate(df):
+            if i < 10:
+                print(row)
+            else:
+                break
+        
