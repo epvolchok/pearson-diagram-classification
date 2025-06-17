1cb89d0215eed5ba7da2c56e3c8c2ebde0592cad
epvolchok <e.p.volchok@gmail.com>
2025-06-17 16:21:56 +0200

    documentation with links

--- documentation/documentation_full.py
+++ documentation/documentation_full.py
@@ -1,5 +1,4 @@
 import os
-import subprocess
 import re
 
 
@@ -38,50 +37,79 @@ module_files = []
 for order_list, module_dir in zip([mlustering_files_order, minteractive_files_order, manalyse_files_order], [MLUSTERING_DIR, MINTERACTIVE_DIR, MANALYSE_DIR]):
     collect_modules(module_files, order_list, module_dir)
 
-html_parts = []
-style_block = ''
-index_file = os.path.join(OUTPUT_HTML_DIR, 'index.html')
 readme_file = os.path.join(OUTPUT_HTML_DIR, 'readme_html.html')
 
-module_files.insert(0, readme_file)
+# insert readme
+def clean_embedded_html(content: str) -> str:
+    """Удаляет <html>, <head>, <body>, <style> и <!DOCTYPE> из вложенного HTML."""
+    content = re.sub(r'<style.*?</style>', '', content, flags=re.DOTALL | re.IGNORECASE)
+    content = re.sub(r'<head.*?</head>', '', content, flags=re.DOTALL | re.IGNORECASE)
+    content = re.sub(r'</?html[^>]*>', '', content, flags=re.IGNORECASE)
+    content = re.sub(r'</?body[^>]*>', '', content, flags=re.IGNORECASE)
+    content = re.sub(r'<!DOCTYPE [^>]+>', '', content, flags=re.IGNORECASE)
+    return content.strip()
 
+def update_image_paths(html_text):
+    """
+    Находит все <img src="figures/..."> и заменяет на <img src="html/figures/...">
+    """
+    return re.sub(
+        r'(<img\s+[^>]*src=")(figures/[^"]+)"',
+        r'\1html/\2"',
+        html_text
+    )
 
-
-style_block = """
-<style>
-  body {
-    font-family: Arial, sans-serif;
-    font-size: 20px;
-    margin: 2em;
-    background: #fdfdfd;
-  }
-  section {
-    margin-bottom: 2em;
-  }
-  iframe {
-    width: 100%;
-    height: 1000px;
-    border: 1px solid #aaa;
-    border-radius: 6px;
-    background: white;
-  }
-  h2 {
-    margin-bottom: 0.5em;
-    font-size: 1.5em;
-  }
-</style>
+with open(readme_file, 'r', encoding='utf-8') as f:
+        content = f.read()
+readme_content = clean_embedded_html(content)
+section_readme = f"""
+<section>
+{update_image_paths(readme_content)}
+</section>
 """
 
-script_resize = """
-<script>
-    function resizeIframe(iframe) {
-      try {
-        iframe.style.height = iframe.contentWindow.document.body.scrollHeight + 'px';
-      } catch (e) {
-        console.warn("⚠️ Не удалось получить высоту iframe:", e);
-      }
+# generate index
+toc_lines = [
+    "<h1>Index</h1>",
+    "<ul>"
+]
+
+module_names = ['Clustering module: features extraction', 'Clustering module: preprocessing', 'Clustering module: clustering', \
+                'Clustering module: service functions', 'Interactive module: service functions', \
+                  'Interactive module: pipeline', 'Visualization']
+
+for name, file in zip(module_names, module_files):
+    link = rel_path = os.path.relpath(file, DOC_DIR).replace(os.sep, "/")
+    toc_lines.append(f'  <li><a href="{link}">{name}</a></li>')
+
+toc_lines.append("</ul>")
+
+# settings
+style_block = """
+<style>
+    body {
+      font-family: sans-serif;
+      font-size: 18px;
+      margin: 2em;
+      background: #fdfdfd;
     }
-  </script>"""
+    section {
+      margin-bottom: 2em;
+    }
+    a:link, a:visited {
+    color: #0066cc;
+    text-decoration: none;
+  }
+
+  @media print {
+    a:link, a:visited {
+      color: #000000;  /* или другой цвет, например тёмно-синий */
+      text-decoration: none;
+    }
+  }
+  </style>
+"""
+
 
 html_head = f'''<!DOCTYPE html>
 <html lang='en'>
@@ -89,28 +117,16 @@ html_head = f'''<!DOCTYPE html>
     <meta charset='UTF-8'>
     <title>Project Documentation</title>
     {style_block}
-    {script_resize}
 </head>
 <body>
 <h1>Project Documentation</h1>
 '''
 html_footer = "</body>\n</html>"
 
-sections = []
-for filename in module_files:
-    with open(filename, 'r', encoding='utf-8') as f:
-        content = f.read()
-    section = f"""
-<section>
-<iframe src="{filename}" onload="resizeIframe(this)"></iframe>
-</section>
-        """
-    sections.append(section)
 
+final_html = html_head + section_readme + ''.join(toc_lines) + html_footer
 
-final_html = html_head + "\n".join(sections) + html_footer
-
-output_path = os.path.join(DOC_DIR, 'full_documentation.html')
+output_path = os.path.join(DOC_DIR, 'documentation_full.html')
 with open(output_path, 'w', encoding='utf-8') as f:
     f.write(final_html)
 
